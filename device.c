bf59e61282b167422127a12b4c5e4471545044df
hamasaki <hamasaki0011@me.com>
2025-03-28 13:51:03 +0900

    CRC algo check #3

--- device.c
+++ device.c
@@ -209,12 +209,17 @@ uint8_t i2c_generate_crc(const uint8_t* data, uint16_t count) {
     for (current_byte = 0; current_byte < count; ++current_byte) {
         crc ^= (data[current_byte]);
         printf("device_#211 original is %02x\n", data[current_byte]);
-        printf("device_#212 before_crc is %02x\n", crc);
+        printf("device_#212 crc_before is %02x\n", crc);
         for (crc_bit = 8; crc_bit > 0; --crc_bit) {
-            if (crc & 0x80)
+            if (crc & 0x80){
                 crc = (crc << 1) ^ CRC8_POLYNOMIAL;
-            else
+                printf("device_#216 crc1 is %02x\n", crc);
+            }
+            else{
                 crc = (crc << 1);
+                printf("device_#220 crc0 is %02x\n", crc);
+            }
+            //printf("device_#218 crc_n is %02x\n", crc);
         }
         printf("device_#219 crc is %02x\n", crc);
     }
